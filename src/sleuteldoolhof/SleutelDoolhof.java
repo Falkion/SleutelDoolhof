1cd2044fd541ee28f258c32a601132545428117a
nebbii <ben.wolthuis@gmail.com>
2018-04-04 23:07:12 +0200

    grid of jlabels worksgit add .

--- src/sleuteldoolhof/SleutelDoolhof.java
+++ src/sleuteldoolhof/SleutelDoolhof.java
@@ -23,9 +23,33 @@ public class SleutelDoolhof {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 650);
         
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(10, 10, 5, 5));
+        int xSize = 10;
+        int ySize = 10;
         
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(xSize, ySize, 5, 5));
+        
+        JLabel gridLabels[][] = new JLabel[10][10];
+        for(int i=0;i<xSize;i++) {
+            for(int j=0;j<ySize;j++) {
+                String label = Integer.toString(i) + Integer.toString(j);
+                System.out.print("X:");
+                System.out.print(i);
+                System.out.print("Y:");
+                System.out.println(j);
+                gridLabels[i][j] = new JLabel(label);
+            }
+        }
+        
+        panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        for(int i=0;i<xSize;i++) {
+            for(int j=0;j<ySize;j++) {
+                panel.add(gridLabels[i][j]);
+            }
+        }
+        
+        
+        frame.add(panel);
    }
     
 }
