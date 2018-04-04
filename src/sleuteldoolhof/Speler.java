f535a0438c15b84d7644dc11f3366b453fb36387
Ben Wolthuis <ben.wolthuis@gmail.com>
2018-04-04 15:48:55 +0200

    added empty speler functions

---
+++ src/sleuteldoolhof/Speler.java
@@ -0,0 +1,26 @@
+package sleuteldoolhof;
+
+public class Speler extends VlakObject
+{
+    private boolean huidigeSleutel;
+    
+    public void setHuidigeSleutel(boolean Sleutel) {
+       this.huidigeSleutel = huidigeSleutel; 
+    }
+
+    public void getHuidigeSleutel() {
+       return this.huidigeSleutel = huidigeSleutel; 
+    }
+
+    public void move() {
+
+    }
+
+    public void SleutelOppakken(boolean huidigeSleutel) {
+
+    }
+
+    public void checkWinCondities(boolean Eindveld) {
+
+    }
+}
